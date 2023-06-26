29cd524d1222bdf178ac59b970cb2963cbd28039
montree <montri3112545@gmail.com>
2023-06-26 21:33:07 +0700

    new app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import { useState } from 'react';
 
 function App() {
+  const [num, setNum] = useState(0);
+
+  const [primeR, setPrime] = useState("");
+
+  function AddPrimeNumber() {
+    let prime = true;
+    let num_x = num + 1;
+    setNum(num_x);
+    if (num_x === 0 || num_x === 1) setPrime(num_x + " ไม่เป็นจำนวนเฉพาะ")
+    else {
+      for (let i = 2; i < num_x; i++) {
+        if (num_x % i === 0) {
+          prime = false;
+          break;
+        }
+      }
+      if (prime) setPrime(num_x + " เป็นจำนวนเฉพาะ");
+      else setPrime(num_x + " ไม่เป็นจำนวนเฉพาะ");
+    }
+  }
+
+  function SubtractPrimeNumber() {
+    let prime = true;
+    let num_x = num - 1;
+    if (num_x >= 0) {
+      setNum(num_x);
+    }
+    if (num_x === 0 || num_x === 1) {
+      setPrime(num_x + " ไม่เป็นจำนวนเฉพาะ");
+    } else {
+      for (let i = 2; i < num_x; i++) {
+        if (num_x % i === 0) {
+          prime = false;
+          break;
+        }
+      }
+      if (prime && num_x >= 1) setPrime(num_x + " เป็นจำนวนเฉพาะ");
+      else setPrime(num_x + " ไม่เป็นจำนวนเฉพาะ");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className='MyContent'>
+        <h2 id="wo">{num}</h2>
+        <button onClick={AddPrimeNumber}>เพิ่ม</button>
+        <button onClick={SubtractPrimeNumber}>ลด</button>
+        <p className="text-center">{primeR}</p>
+      </div>
+      <Footer />
     </div>
+
+
   );
 }
 
